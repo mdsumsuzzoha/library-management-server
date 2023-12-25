14a65f0264239d494d984f1ed45c2612372235ed
Md. Sumsuzzoha <mdzoha11@gmail.com>
2023-12-25 20:32:38 +0600

    books delete operations

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ async function run() {
                 const result = await booksCollection.findOneAndUpdate(query, update, options);
 
                 if (result.value) {
-                    res.send(result.value);
+                    res.send(result);
                 } else {
                     return res.status(404).send({ message: 'Book not found' });
                 }
@@ -217,7 +217,44 @@ async function run() {
                 const result = await booksCollection.findOneAndUpdate(query, update, options);
 
                 if (result.value) {
-                    res.send(result.value);
+                    res.send(result);
+                } else {
+                    return res.status(404).send({ message: 'Book not found' });
+                }
+            } catch (error) {
+                // console.error('Error in increasing book quantity:', error);
+                return res.status(500).send({ message: 'Internal server error' });
+            }
+        });
+        app.put('/books/:_id', async (req, res) => {
+            try {
+                const bookId = req.params._id
+                const updateBook = req.body;
+                // console.log(bookId);
+                // console.log(updateBook);
+                const query = { _id: new ObjectId(bookId) };
+                const options = { upsert: true };
+                const updateDoc = {
+                    $set: {
+                        name: updateBook.name,
+                        author: updateBook.author,
+                        category: updateBook.category,
+                        img: updateBook.img,
+                        description: updateBook.description,
+                        qty: updateBook.qty, // Assuming qty is a number
+                        rating: updateBook.rating, // Assuming rating is a floating-point number
+                        ISBN: updateBook.ISBN,
+                        pages: updateBook.pages,
+                        publisher: updateBook.publisher,
+                        language: updateBook.language
+                        // Add other fields as needed
+                    },
+                };
+                const result = await booksCollection.updateOne(query, updateDoc, options);
+                console.log(result);
+
+                if (result.value) {
+                    // res.send(result);
                 } else {
                     return res.status(404).send({ message: 'Book not found' });
                 }
@@ -227,7 +264,44 @@ async function run() {
             }
         });
 
+        // app.delete('/allBooks/:id', verifyToken, async (req, res) => {
+        //     try {
+        //         const id = req.params.id;
+        //         console.log(id)
+        //         const query = { _id: new ObjectId(_id) };
+        //         console.log(query)
+        //         const result = await booksCollection.deleteOne(query);
+        //         console.log(result);
+        //         if (result.deletedCount === 1) {
+        //             // res.send(result);
+        //         } else {
+        //             return res.status(404).send({ message: 'Item not found' });
+        //         }
 
+        //     } catch (error) {
+        //         // console.error('Error in deleting item:', error);
+        //         return res.status(500).send({ message: 'Internal server error' });
+        //     }
+
+        // })
+
+        app.delete('/allBooks/:id', async (req, res) => {
+            try {
+                const id = req.params.id; // Access the 'id' parameter correctly
+                const query = { _id: new ObjectId(id) };
+                console.log()
+                const result = await booksCollection.deleteOne(query);
+
+                if (result.deletedCount > 0) {
+                    res.send(result);
+                } else {
+                    return res.status(404).send({ message: 'Item not found' });
+                }
+            } catch (error) {
+                // console.error('Error in deleting item:', error);
+                return res.status(500).send({ message: 'Internal server error' });
+            }
+        });
         app.delete('/borrowed/:id', async (req, res) => {
             try {
                 const id = req.params.id; // Access the 'id' parameter correctly
@@ -235,7 +309,7 @@ async function run() {
                 const result = await borrowCollection.deleteOne(query);
 
                 if (result.deletedCount === 1) {
-                    res.send({ message: 'Item deleted successfully' });
+                    res.send(result);
                 } else {
                     return res.status(404).send({ message: 'Item not found' });
                 }
